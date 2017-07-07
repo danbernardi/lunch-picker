b296c154f99ba21e6ae8740bfbcc2fe96115bbef
Daniel <danbernardi1@gmail.com>
2017-07-07 15:03:35 -0700

    Adds physics experiements

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,61 +4,68 @@ const engine = new BABYLON.Engine(canvas, true);
 const createScene = () => {
   const scene = new BABYLON.Scene(engine);
   scene.clearColor = new BABYLON.Color3(0, 0, 0);
+  scene.enablePhysics();
 
-  var camera = new BABYLON.ArcRotateCamera('camera', 30, 1.2, 15, BABYLON.Vector3.Zero(), scene);
-  camera.upperRadiusLimit = 50;
+  var camera = new BABYLON.ArcRotateCamera('camera', 30, 1.4, 70, BABYLON.Vector3.Zero(), scene);
+  camera.upperRadiusLimit = 100;
   camera.lowerRadiusLimit = 8;
+  camera.upperBetaLimit = 1.6;
   camera.attachControl(canvas);
 
-  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-  light.intensity = 0.5;
+  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(2, 1, 0), scene);
+  light.intensity = 0.7;
 
-  const sun = BABYLON.Mesh.CreateSphere('sphere1', 16, 5, scene);
-  const sunMaterial = new BABYLON.StandardMaterial('sunMaterial', scene);
-  sunMaterial.emissiveTexture = new BABYLON.Texture('assets/sun.jpg', scene);
-  sunMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-  sunMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-  sun.material = sunMaterial;
-  sun.position.y = 1;
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-  const sunLight = new BABYLON.PointLight('sunlight', BABYLON.Vector3.Zero(), scene);
-  sunLight.intensity = 6;
+  const ground = BABYLON.Mesh.CreateGround('ground', 400, 400, 2, scene);
+  ground.position.y = -7.5;
+  ground.position.x = 0;
+  ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 5 }, scene);
 
-  const planet1 = new BABYLON.Mesh.CreateSphere('planet1', 16, 1, scene);
-  const planet1Material = new BABYLON.StandardMaterial('planetMat1', scene);
-  planet1Material.diffuseTexture = new BABYLON.Texture('assets/planet1.jpg', scene);
-  planet1Material.specularColor = new BABYLON.Color3(1, 1, 1);
-  planet1.material = planet1Material;
-  planet1.position.x = 10;
-  planet1.orbit = {
-    radius: planet1.position.x,
-    speed: 0.01,
-    angle: 0
-  };
+  function generateBoxes(count) {
+    for (let i = 0; i < count; i++) {
+      const mesh = BABYLON.Mesh.CreateBox(`box${i}`, getRandomInt(0.5, 3.0), scene);
+      mesh.position.y = getRandomInt(5, 20);
+      mesh.position.x = getRandomInt(-10, 10);
+      mesh.position.z = getRandomInt(-10, 10);
+      mesh.rotation.x = Math.PI/getRandomInt(0.01, 90);
+      mesh.rotation.y = Math.PI/getRandomInt(0.01, 90);
+      mesh.physicsImposter = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.BoxImpostor, { friction: 0.3, mass: 3, restitution: getRandomInt(0.07, 1) }, scene);
+      mesh.actionManager = new BABYLON.ActionManager(scene);
+    };
+  }
 
-  const planet2 = new BABYLON.Mesh.CreateSphere('planet2', 16, 1.3, scene);
-  const planet2Material = new BABYLON.StandardMaterial('planetMat2', scene);
-  planet2Material.diffuseTexture = new BABYLON.Texture('assets/planet2.jpg', scene);
-  planet2Material.specularColor = new BABYLON.Color3(1, 1, 1);
-  planet2.material = planet2Material;
-  planet2.position.x = 5;
-  planet2.orbit = {
-    radius: planet2.position.x,
-    speed: -0.01,
-    angle: 0.4
-  };
+  function generateSpheres(count) {
+    for (let i = 0; i < count; i++) {
+      const mesh = BABYLON.Mesh.CreateSphere(`sphere${i}`, 16, getRandomInt(0.5, 3.0), scene);
+      mesh.position.y = getRandomInt(5, 20);
+      mesh.position.x = getRandomInt(-10, 10);
+      mesh.position.z = getRandomInt(-10, 10);
+      mesh.rotation.x = Math.PI/getRandomInt(0.01, 90);
+      mesh.rotation.y = Math.PI/getRandomInt(0.01, 90);
+      mesh.physicsImposter = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.SphereImpostor, { friction: 0.3, mass: 3, restitution: getRandomInt(0.07, 1) }, scene);
+      mesh.actionManager = new BABYLON.ActionManager(scene);
+    };
+  }
 
-  const planet3 = new BABYLON.Mesh.CreateSphere('planet3', 16, 0.4, scene);
-  const planet3Material = new BABYLON.StandardMaterial('planetMat3', scene);
-  planet3Material.diffuseTexture = new BABYLON.Texture('assets/planet3.jpg', scene);
-  planet3Material.specularColor = new BABYLON.Color3(1, 1, 1);
-  planet3.material = planet3Material;
-  planet3.position.x = 8;
-  planet3.orbit = {
-    radius: planet3.position.x,
-    speed: 0.012,
-    angle: 0
-  };
+  function generateCylinders(count) {
+    for (let i = 0; i < count; i++) {
+      const mesh = BABYLON.Mesh.CreateCylinder(`cylinder${i}`, getRandomInt(0.6, 3), getRandomInt(0.6, 3), getRandomInt(0.6, 3), 16, 1, scene);
+      mesh.position.y = getRandomInt(5, 20);
+      mesh.position.x = getRandomInt(-10, 10);
+      mesh.position.z = getRandomInt(-10, 10);
+      mesh.rotation.x = Math.PI/getRandomInt(0.01, 90);
+      mesh.rotation.y = Math.PI/getRandomInt(0.01, 90);
+      mesh.physicsImposter = new BABYLON.PhysicsImpostor(mesh, BABYLON.PhysicsImpostor.CylinderImpostor, { friction: 0.3, mass: 3, restitution: getRandomInt(0.07, 1) }, scene);
+      mesh.actionManager = new BABYLON.ActionManager(scene);
+    };
+  }
+
+  generateBoxes(50);
+  generateSpheres(50);
+  generateCylinders(50);
 
   // skybox
   const skybox = BABYLON.Mesh.CreateBox('skybox', 1000, scene);
@@ -68,18 +75,8 @@ const createScene = () => {
   skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
   skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture('assets/skybox', scene);
   skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-
   skybox.infiniteDistance = true;
   skybox.material = skyboxMaterial;
-
-  scene.beforeRender = () => {
-    [planet1, planet2, planet3].forEach((planet) => {
-      planet.position.x = planet.orbit.radius * Math.sin(planet.orbit.angle);
-      planet.position.z = planet.orbit.radius * Math.cos(planet.orbit.angle);
-
-      planet.orbit.angle += planet.orbit.speed;
-    });
-  }
 
   return scene;
 }
