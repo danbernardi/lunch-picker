08649b5f993173c4a55b43c724b0350481d883fe
Daniel <danbernardi1@gmail.com>
2017-07-28 12:07:35 -0700

    Add flavor to lunch options

--- src/js/data/tileData.js
+++ src/js/data/tileData.js
@@ -1,29 +1,29 @@
 export default [
-  { x: 8, z: -24, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'Indian buffet' },
+  { x: 8, z: -24, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'an Indian buffet' },
   { x: 0, z: -24, color: new BABYLON.Color3(0, 0.25, 1), lunchPlace: 'The Plant' },
-  { x: -8, z: -24, color: new BABYLON.Color3(0, 0, 0.75), lunchPlace: 'Ramen Bar' },
+  { x: -8, z: -24, color: new BABYLON.Color3(0, 0, 0.75), lunchPlace: 'the Ramen Bar' },
 
   { x: 8, z: -16, color: new BABYLON.Color3(0.75, 1, 0), lunchPlace: 'Orale Orale' },
   { x: 0, z: -16, color: new BABYLON.Color3(0.25, 1, 0), lunchPlace: 'Escape From New York Pizza' },
-  { x: -8, z: -16, color: new BABYLON.Color3(0, 0.75, 0), lunchPlace: 'Ferry Building' },
+  { x: -8, z: -16, color: new BABYLON.Color3(0, 0.75, 0), lunchPlace: 'the Ferry Building' },
 
-  { x: 8, z: -8, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'Rosa Mexicano' },
-  { x: 0, z: -8, color: new BABYLON.Color3(0, 0.25, 1), lunchPlace: 'Post Office' },
+  { x: 8, z: -8, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'Rosa Mexicano. Fancy. Also expensive.' },
+  { x: 0, z: -8, color: new BABYLON.Color3(0, 0.25, 1), lunchPlace: 'the Post Office' },
   { x: -8, z: -8, color: new BABYLON.Color3(0, 0, 0.75), lunchPlace: 'HEYDAY' },
 
   { x: 8, z: 0, color: new BABYLON.Color3(0.75, 1, 0), lunchPlace: 'Tadich Grill' },
   { x: 0, z: 0, color: new BABYLON.Color3(0.25, 1, 0), lunchPlace: 'Mixt Greens' },
   { x: -8, z: 0, color: new BABYLON.Color3(0, 0.75, 0), lunchPlace: 'The Slanted Door' },
 
-  { x: 8, z: 8, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'Fog City' },
+  { x: 8, z: 8, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'Fog City, hope you like walking.' },
   { x: 0, z: 8, color: new BABYLON.Color3(0, 0.25, 1), lunchPlace: 'Oasis Grill' },
   { x: -8, z: 8, color: new BABYLON.Color3(0, 0, 0.75), lunchPlace: 'Stone Korean Kitchen' },
 
   { x: 8, z: 16, color: new BABYLON.Color3(0.75, 1, 0), lunchPlace: 'Le Regency' },
-  { x: 0, z: 16, color: new BABYLON.Color3(0.25, 1, 0), lunchPlace: 'Pizza' },
-  { x: -8, z: 16, color: new BABYLON.Color3(0, 0.75, 0), lunchPlace: 'Pizza' },
+  { x: 0, z: 16, color: new BABYLON.Color3(0.25, 1, 0), lunchPlace: 'eat Pizza, again.' },
+  { x: -8, z: 16, color: new BABYLON.Color3(0, 0.75, 0), lunchPlace: 'eat Pizza, fantastic.' },
 
-  { x: 8, z: 24, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'Pizza' },
-  { x: 0, z: 24, color: new BABYLON.Color3(0, 0.25, 1), lunchPlace: 'Pizza' },
-  { x: -8, z: 24, color: new BABYLON.Color3(0, 0, 0.75), lunchPlace: 'Pizza' }
+  { x: 8, z: 24, color: new BABYLON.Color3(0, 0.75, 1), lunchPlace: 'eat Pizza. Sorry Sasha.' },
+  { x: 0, z: 24, color: new BABYLON.Color3(0, 0.25, 1), lunchPlace: 'eat Pizza, enjoy.' },
+  { x: -8, z: 24, color: new BABYLON.Color3(0, 0, 0.75), lunchPlace: 'eat Pizza' }
 ];
